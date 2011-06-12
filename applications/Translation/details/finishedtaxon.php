16137d131b17e3f804f2dd054021a343f4865292
saraelshobaky <sara.elshobaky@bibalex.org>
2011-06-12 14:57:55 +0300

    Thank you after finishing taxon

--- applications/Translation/details/finishedtaxon.php
+++ applications/Translation/details/finishedtaxon.php
@@ -5,22 +5,26 @@
 <?php
 	if($process==2){
 	?>
-	Translation of <a target="_blank" href="<?=$eol_site_url?>/pages/<?=$taxon->id?>"><?=$taxon->scientificName?></a> has been completed. 
+	Thank you for your effort
+	<br/><br/>Translation of <a target="_blank" href="<?=$eol_site_url?>/pages/<?=$taxon->id?>"><?=$taxon->scientificName?></a> has been completed. 
 	<br/><br/>No further modifications are allowed. 
 	<br/><br/>Now the translated material will be submitted to Linguistic
 Review. 
 <?php }else if($process==3){?>
-	Linguistic review of <a target="_blank" href="<?=$eol_site_url?>/pages/<?=$taxon->id?>"><?=$taxon->scientificName?></a> has been completed. 
+	Thank you for your effort
+	<br/><br/>Linguistic review of <a target="_blank" href="<?=$eol_site_url?>/pages/<?=$taxon->id?>"><?=$taxon->scientificName?></a> has been completed. 
 	<br/><br/>No further modifications are allowed. 
 	<br/><br/>Now the linguistically reviewed material will be submitted to
 Scientific Review.
 <?php }else if($process==4){?>
-	Scientific review of <a target="_blank" href="<?=$eol_site_url?>/pages/<?=$taxon->id?>"><?=$taxon->scientificName?></a> has been completed. 
+	Thank you for your effort
+	<br/><br/>Scientific review of <a target="_blank" href="<?=$eol_site_url?>/pages/<?=$taxon->id?>"><?=$taxon->scientificName?></a> has been completed. 
 	<br/><br/>No further modifications are allowed. 
 	<br/><br/>Now the scientifically reviewed material will be submitted to the
 final editing phase.
 <?php }else if($process==5){?>
-	Final Editing of <a target="_blank" href="<?=$eol_site_url?>/pages/<?=$taxon->id?>"><?=$taxon->scientificName?></a> has been completed. 
+	Thank you for your effort
+	<br/><br/>Final Editing of <a target="_blank" href="<?=$eol_site_url?>/pages/<?=$taxon->id?>"><?=$taxon->scientificName?></a> has been completed. 
 	<br/><br/>No further modifications are allowed. 
 	<br/><br/>Now the final editing material will be waiting to be published.
 <?php }?>
