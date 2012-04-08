1825cadac948b4710c94348b85230216b866a72e
saraelshobaky <sara.elshobaky@bibalex.org>
2012-04-08 10:59:48 +0200

    Solve conflict

--- db_migration/7-increase_size_of_RH_RS_Title_Location.sql
+++ db_migration/7-increase_size_of_RH_RS_Title_Location.sql
@@ -2,5 +2,5 @@ ALTER TABLE `a_data_objects` CHANGE COLUMN `object_title` `object_title` VARCHAR
 ALTER TABLE `archived_a_data_objects` CHANGE COLUMN `object_title` `object_title` VARCHAR(500) NULL DEFAULT NULL  , CHANGE COLUMN `rights_statement` `rights_statement` VARCHAR(500) NULL DEFAULT NULL  , CHANGE COLUMN `rights_holder` `rights_holder` VARCHAR(500) NULL DEFAULT NULL  ;
 ALTER TABLE `a_data_objects` CHANGE COLUMN `location` `location` VARCHAR(500) NULL DEFAULT NULL  ;
 ALTER TABLE `archived_a_data_objects` CHANGE COLUMN `location` `location` VARCHAR(500) NULL DEFAULT NULL  ;
-
+--Conflict Test
 
